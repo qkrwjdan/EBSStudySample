be8e97e257d28514fe1e4fd2ba72ae33f3166a3a
qkrwjdan <madogisa12@naver.com>
2022-06-10 00:38:10 +0900

    new page

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,8 @@ app.get('/new_page', (req, res) => {
     res.send({text : "It's new page!"});
 })
 
-
-app.get('/new_page/two', (req, res) => {
-    res.send({text : "It's new page!", version: "second"});
+app.get('/new', (req, res) => {
+    res.send({text : "이게 되네..."});
 })
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
